866f0c04a3728acc82169a7f264f2b13e155cd8a
Paola Garcia <pacri14@gmail.com>
2021-08-25 17:49:44 +0000

    Code refactored, clientjs created to separate connection code

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require("net");
+var { connect } = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
- 
-  conn.on('data', (data) => {
-    console.log("Data! ",data)
-  })
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+console.log("Connecting2 ...");
+connect()
